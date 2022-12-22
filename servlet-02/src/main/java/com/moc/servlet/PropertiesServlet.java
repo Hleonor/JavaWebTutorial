0fde61905cb1a48f79679b324cb73a11123df201
moc <1287607885@qq.com>
2022-12-22 22:48:57 +0800

    读取资源文件

---
+++ servlet-02/src/main/java/com/moc/servlet/PropertiesServlet.java
@@ -0,0 +1,13 @@
+package com.moc.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+
+import java.util.Properties;
+
+public class PropertiesServlet extends HttpServlet
+{
+    public void test()
+    {
+        Properties properties = new Properties();
+    }
+}
