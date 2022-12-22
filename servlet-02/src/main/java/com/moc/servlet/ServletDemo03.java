784801a92f21fe1cc2bfe8e7f41192ac022858ab
moc <1287607885@qq.com>
2022-12-22 22:26:57 +0800

    获取参数

---
+++ servlet-02/src/main/java/com/moc/servlet/ServletDemo03.java
@@ -0,0 +1,27 @@
+package com.moc.servlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class ServletDemo03 extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        ServletContext servletContext = this.getServletContext();
+
+        String url = servletContext.getInitParameter("url");
+        resp.getWriter().print(url);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        doGet(req, resp);
+    }
+}
